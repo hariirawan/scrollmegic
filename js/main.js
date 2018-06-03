2a212e0cfbb705f3bbf987deaff036db12f01e4e
hari irawa <hari31121993@gmail.com>
2018-06-03 15:07:29 +0700

    Duration and TriggerHooks

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
    var controller = new ScrollMagic.Controller();
    var ourScene = new ScrollMagic.Scene({
-      triggerElement: '#project01'
+      triggerElement: '#project01',
+      duration: '100%',
+      triggerHook: 0.6
    })
       .setClassToggle('#project01', 'fade-in')
-      .addIndicators()
+      .addIndicators({
+         name: 'fade scene',
+         colorTrigger: 'black',
+         colorStart: '#75C695',
+         colorEnd: 'pink'
+      })
       .addTo(controller);
 });
